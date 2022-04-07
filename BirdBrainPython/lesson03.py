b79d4698d58e72a56dc92c6e9f4fe8b5864c5c1b
Shawn F <shawn@TheHiddsProject.hsd1.ca.comcast.net>
2022-04-06 18:06:59 -0700

    add lesson03.py

--- BirdBrainPython/lesson03.py
+++ BirdBrainPython/lesson03.py
@@ -2,7 +2,43 @@ from BirdBrain import Finch
 from time import sleep
 bird = Finch()
 
-def exersice1():
-    bird.setBeak(100,5,0)
-    sleep(4)
+def exercise1():
+    bird.setBeak(10,5,7)
+    sleep(6)
     bird.setBeak(0,0,0)
+
+def exercise2():
+    bird.setBeak(100,0,100)
+    sleep(3)
+    bird.setBeak(0,100,100)
+    sleep(3)
+    bird.setBeak(100,100,0)
+    sleep(2)
+    bird.stopAll()
+
+def exercise3():
+    bird.setTail(1,0,0,100)
+    bird.setTail(4,0,0,100)
+    sleep(2)
+    bird.stopAll()
+
+def exercise4():
+    bird.setBeak(100,0,0)
+    bird.setTail(1,100,64,0)
+    bird.setTail(2,100,100,0)
+    bird.setTail(3,0,100,0)
+    bird.setTail(4,0,0,100)
+    sleep(5)
+    bird.stopAll()
+
+def exercise5():
+    userResponse = input("which tail light 1-4 should be red? ")
+    number = int(userResponse)
+    bird.setTail(number,100,0,0)
+    sleep(1)
+    bird.stopAll()
+
+def exercise6():
+    bird.setTail(all,100,100,100)
+
+exercise6()
